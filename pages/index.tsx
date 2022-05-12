0ecf5b57431c0aafc3bb0f935feb8597193066e6
Guðmundur Snorri <gudmundursig@nova.is>
2022-05-12 17:43:30 +0000

    sdf

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,6 +45,7 @@ const Home: NextPage = () => {
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap"
         rel="stylesheet"
       />
+      <link href="/fonts/" rel="stylesheet" />
       <link
         rel="preload"
         href="/fonts/Akkordeon-Eleven.otf"
